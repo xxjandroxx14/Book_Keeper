4947a7cd93ca88f677ec8a72375ae165bc060950
Elaisa Mae Millare <Elaisa Mae Millare@DESKTOP-06QM1SP>
2025-11-30 14:49:25 +0800

    Add project files.

---
+++ Book_Keeper/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ButtonConnect = New Button()
+        ButtonUpdate = New Button()
+        ButtonDelete = New Button()
+        ButtonCreate = New Button()
+        TextBoxId = New TextBox()
+        TextBoxCategory = New TextBox()
+        TextBoxTitle = New TextBox()
+        TextBoxAuthor = New TextBox()
+        TextBoxAvailability = New TextBox()
+        DataGridView1 = New DataGridView()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        ButtonRead = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ButtonConnect
+        ' 
+        ButtonConnect.Location = New Point(260, 39)
+        ButtonConnect.Name = "ButtonConnect"
+        ButtonConnect.Size = New Size(76, 23)
+        ButtonConnect.TabIndex = 0
+        ButtonConnect.Text = "Connect"
+        ButtonConnect.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonUpdate
+        ' 
+        ButtonUpdate.Location = New Point(544, 39)
+        ButtonUpdate.Name = "ButtonUpdate"
+        ButtonUpdate.Size = New Size(75, 23)
+        ButtonUpdate.TabIndex = 1
+        ButtonUpdate.Text = "Update"
+        ButtonUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDelete
+        ' 
+        ButtonDelete.Location = New Point(625, 39)
+        ButtonDelete.Name = "ButtonDelete"
+        ButtonDelete.Size = New Size(75, 23)
+        ButtonDelete.TabIndex = 2
+        ButtonDelete.Text = "Delete"
+        ButtonDelete.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonCreate
+        ' 
+        ButtonCreate.Location = New Point(361, 39)
+        ButtonCreate.Name = "ButtonCreate"
+        ButtonCreate.Size = New Size(75, 23)
+        ButtonCreate.TabIndex = 3
+        ButtonCreate.Text = "Create"
+        ButtonCreate.UseVisualStyleBackColor = True
+        ' 
+        ' TextBoxId
+        ' 
+        TextBoxId.Location = New Point(113, 56)
+        TextBoxId.Name = "TextBoxId"
+        TextBoxId.Size = New Size(100, 23)
+        TextBoxId.TabIndex = 4
+        TextBoxId.Visible = False
+        ' 
+        ' TextBoxCategory
+        ' 
+        TextBoxCategory.Location = New Point(113, 206)
+        TextBoxCategory.Name = "TextBoxCategory"
+        TextBoxCategory.Size = New Size(100, 23)
+        TextBoxCategory.TabIndex = 5
+        ' 
+        ' TextBoxTitle
+        ' 
+        TextBoxTitle.Location = New Point(113, 103)
+        TextBoxTitle.Name = "TextBoxTitle"
+        TextBoxTitle.Size = New Size(100, 23)
+        TextBoxTitle.TabIndex = 6
+        ' 
+        ' TextBoxAuthor
+        ' 
+        TextBoxAuthor.Location = New Point(113, 153)
+        TextBoxAuthor.Name = "TextBoxAuthor"
+        TextBoxAuthor.Size = New Size(100, 23)
+        TextBoxAuthor.TabIndex = 7
+        ' 
+        ' TextBoxAvailability
+        ' 
+        TextBoxAvailability.Location = New Point(113, 256)
+        TextBoxAvailability.Name = "TextBoxAvailability"
+        TextBoxAvailability.Size = New Size(100, 23)
+        TextBoxAvailability.TabIndex = 8
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(244, 89)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(456, 332)
+        DataGridView1.TabIndex = 9
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(36, 153)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(44, 15)
+        Label2.TabIndex = 11
+        Label2.Text = "Author"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(36, 103)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(30, 15)
+        Label3.TabIndex = 12
+        Label3.Text = "Title"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(36, 206)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(55, 15)
+        Label4.TabIndex = 13
+        Label4.Text = "Category"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(36, 256)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(65, 15)
+        Label5.TabIndex = 14
+        Label5.Text = "Availability"
+        ' 
+        ' ButtonRead
+        ' 
+        ButtonRead.Location = New Point(454, 39)
+        ButtonRead.Name = "ButtonRead"
+        ButtonRead.Size = New Size(75, 23)
+        ButtonRead.TabIndex = 15
+        ButtonRead.Text = "Read"
+        ButtonRead.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ButtonRead)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(DataGridView1)
+        Controls.Add(TextBoxAvailability)
+        Controls.Add(TextBoxAuthor)
+        Controls.Add(TextBoxTitle)
+        Controls.Add(TextBoxCategory)
+        Controls.Add(TextBoxId)
+        Controls.Add(ButtonCreate)
+        Controls.Add(ButtonDelete)
+        Controls.Add(ButtonUpdate)
+        Controls.Add(ButtonConnect)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents ButtonUpdate As Button
+    Friend WithEvents ButtonDelete As Button
+    Friend WithEvents ButtonCreate As Button
+    Friend WithEvents TextBoxId As TextBox
+    Friend WithEvents TextBoxCategory As TextBox
+    Friend WithEvents TextBoxTitle As TextBox
+    Friend WithEvents TextBoxAuthor As TextBox
+    Friend WithEvents TextBoxAvailability As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ButtonRead As Button
+
+End Class
---
+++ Book_Keeper/Form1.vb
@@ -0,0 +1,132 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data
+Public Class Form1
+    Private Const CONN_STRING As String = "server=localhost; userid=root; password=root;database=new_client;"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ButtonRead_Click(Nothing, EventArgs.Empty)
+    End Sub
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                MessageBox.Show("Connected")
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    Private Sub ButtonCreate_Click(sender As Object, e As EventArgs) Handles ButtonCreate.Click
+        If String.IsNullOrWhiteSpace(TextBoxTitle.Text) OrElse String.IsNullOrWhiteSpace(TextBoxAuthor.Text) OrElse String.IsNullOrWhiteSpace(TextBoxCategory.Text) OrElse String.IsNullOrWhiteSpace(TextBoxAvailability.Text) Then
+            MessageBox.Show("Please enter a value in all required fields!", "Missing Value", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+        Dim query As String = "INSERT INTO new_client.client_table ( title,author,category,availability) VALUES ( @title, @author, @category, @availability);"
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@title", TextBoxTitle.Text)
+                    cmd.Parameters.AddWithValue("@author", TextBoxAuthor.Text)
+                    cmd.Parameters.AddWithValue("@category", TextBoxCategory.Text)
+                    cmd.Parameters.AddWithValue("@availability", TextBoxAvailability.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record insert successfully!")
+                    TextBoxId.Clear()
+                    TextBoxTitle.Clear()
+                    TextBoxAuthor.Clear()
+                    TextBoxCategory.Clear()
+                    TextBoxAvailability.Clear()
+                    ButtonRead_Click(Nothing, EventArgs.Empty)
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        Dim query As String = "SELECT * FROM new_client.client_table;"
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                DataGridView1.DataSource = table
+                If DataGridView1.Columns.Contains("id") Then
+                    DataGridView1.Columns("id").Visible = False
+                End If
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentDoubleClick
+        If e.RowIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+            TextBoxTitle.Text = selectedRow.Cells("title").Value.ToString()
+            TextBoxAuthor.Text = selectedRow.Cells("author").Value.ToString()
+            TextBoxCategory.Text = selectedRow.Cells("category").Value.ToString()
+            TextBoxAvailability.Text = selectedRow.Cells("availability").Value.ToString()
+            TextBoxId.Text = selectedRow.Cells("id").Value.ToString()
+        End If
+    End Sub
+    Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
+        Dim query As String = "UPDATE new_client.client_table SET title = @title, author = @author, category = @category, availability = @availability WHERE id = @id;"
+        If String.IsNullOrWhiteSpace(TextBoxId.Text) Then
+            MessageBox.Show("Please select a record to update first by double-clicking the grid.")
+            Return
+        End If
+        If String.IsNullOrWhiteSpace(TextBoxTitle.Text) Then
+            MessageBox.Show("Please enter a title value before updating!", "Missing Value", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBoxTitle.Focus()
+            Return
+        End If
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxId.Text))
+                    cmd.Parameters.AddWithValue("@title", TextBoxTitle.Text)
+                    cmd.Parameters.AddWithValue("@author", TextBoxAuthor.Text)
+                    cmd.Parameters.AddWithValue("@category", TextBoxCategory.Text)
+                    cmd.Parameters.AddWithValue("@availability", TextBoxAvailability.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record updated successfully!")
+                    ButtonRead_Click(Nothing, EventArgs.Empty)
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Error updating record: " & ex.Message)
+        End Try
+    End Sub
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        Dim query As String = "DELETE FROM new_client.client_table WHERE id = @id;"
+        If String.IsNullOrWhiteSpace(TextBoxId.Text) Then
+            MessageBox.Show("Please select a record to delete first by double-clicking the grid.")
+            Return
+        End If
+        If MessageBox.Show("Are you sure you want to delete this record?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.No Then
+            Return
+        End If
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxId.Text))
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record deleted successfully!")
+                    TextBoxId.Clear()
+                    TextBoxTitle.Clear()
+                    TextBoxAuthor.Clear()
+                    TextBoxCategory.Clear()
+                    TextBoxAvailability.Clear()
+                    ButtonRead_Click(Nothing, EventArgs.Empty)
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Error deleting record: " & ex.Message)
+        End Try
+    End Sub
+
+
+End Class
+
